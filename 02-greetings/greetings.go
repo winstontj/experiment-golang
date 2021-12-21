68940454da35582c7da3737b0ec0dfc61f19489f
Winston Tj <winston.tjandra@gmail.com>
2021-12-21 23:59:26 +0900

    feature: E-02 use Hour of the day to determine greetings

--- 02-greetings/greetings.go
+++ 02-greetings/greetings.go
@@ -1,7 +1,24 @@
 package greetings
 
+import (
+	"time"
+)
+
+// Get greetings based on time. Return the greetings in Japanese
+func greetings_by_time() string {
+	now := time.Now()
+	greeting := "こんにちは"
+	currentHour := now.Hour()
+	if currentHour > 4 && currentHour < 12 {
+		greeting = "おはようございます"
+	} else if currentHour > 18 || currentHour < 4 {
+		greeting = "こんばんは"
+	}
+	return greeting
+}
+
 // Returns a greeting to a person in Japanese
 func Hello(name string) string {
-	message := "こんにちは " + name + "様"
+	message := greetings_by_time() + name + "様"
 	return message
 }
